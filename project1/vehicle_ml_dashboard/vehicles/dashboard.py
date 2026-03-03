2fc5adde9b7ce4f8ec66c0f5152aceb290a531cc
Mchrispin <mchrispin14@gmail.com>
2026-03-03 15:10:36 +0200

    Add visualizations: Sunburst, Treemap, Icicle, Manufacturer Bar Chart, Global Sales Map

--- project1/vehicle_ml_dashboard/vehicles/dashboard.py
+++ project1/vehicle_ml_dashboard/vehicles/dashboard.py
@@ -53,3 +53,95 @@ def pivot_table(df):
         float_format='%.2f',
         justify='center'
     )
+
+
+def visualize_sales_with_sunburst_chart(df, height=800):
+    fig = px.sunburst(df, path=['manufacturer', 'fuel_type', 'body_type'], values='selling_price')
+    fig.update_traces(textinfo='label+value')
+    fig.update_layout(height=height)
+    return opy.plot(fig, auto_open=False, output_type='div')
+
+def visualize_sales_with_treemap(df, height=800):
+    df_grouped = df.groupby(
+        ['manufacturer', 'fuel_type', 'body_type'],
+        as_index=False
+    )['selling_price'].sum()
+
+    fig = px.treemap(
+        df_grouped,
+        path=['manufacturer', 'fuel_type', 'body_type'],
+        values='selling_price',
+        title="Sales Distribution Treemap"
+    )
+
+    fig.update_layout(
+        height=height,
+        margin=dict(t=40, l=0, r=0, b=0)
+    )
+
+    return opy.plot(fig, auto_open=False, output_type='div')
+
+def visualize_manufacturer_bar_chart(df, height=600):
+    manufacturer_counts = df['manufacturer'].value_counts().reset_index()
+    manufacturer_counts.columns = ['Manufacturer', 'Count']
+
+    fig = px.bar(
+        manufacturer_counts,
+        x='Manufacturer',
+        y='Count',
+        title="Vehicle Count per Manufacturer",
+        text='Count'
+    )
+
+    fig.update_layout(
+        height=height,
+        xaxis_title="Manufacturer",
+        yaxis_title="Number of Vehicles",
+        margin=dict(t=40, l=0, r=0, b=0)
+    )
+
+    fig.update_traces(textposition='outside')
+
+    return opy.plot(fig, auto_open=False, output_type='div')
+
+def visualize_sales_with_icicle(df, height=800):
+    df_grouped = df.groupby(
+        ['manufacturer', 'fuel_type', 'body_type'],
+        as_index=False
+    )['selling_price'].sum()
+
+    fig = px.icicle(
+        df_grouped,
+        path=['manufacturer', 'fuel_type', 'body_type'],
+        values='selling_price',
+        title="Sales Distribution Icicle Chart"
+    )
+
+    fig.update_layout(
+        height=height,
+        margin=dict(t=40, l=0, r=0, b=0)
+    )
+
+    return opy.plot(fig, auto_open=False, output_type='div')
+
+def sales_by_country(df, height=600):
+    country_sales = df.groupby('client_country')['selling_price'].sum().reset_index()
+    country_sales.columns = ['Country', 'Total Sales']
+    fig = px.choropleth(
+        country_sales,
+        locations='Country',
+        locationmode='country names',
+        color='Total Sales',
+        color_continuous_scale='Viridis',
+        title='Total Sales by Country',
+    )
+    fig.add_scattergeo(
+        locations=country_sales['Country'],
+        locationmode='country names',
+        text=country_sales['Country'],
+        mode='text',
+        textfont=dict(size=9, color='white'),
+        showlegend=False,
+    )
+    fig.update_layout(height=height, geo=dict(showframe=False, projection_type='natural earth'))
+    return opy.plot(fig, auto_open=False, output_type='div')
--- project1/vehicle_ml_dashboard/vehicles/views.py
+++ project1/vehicle_ml_dashboard/vehicles/views.py
@@ -5,7 +5,18 @@ import os
 # Create your views here.
 import pandas as pd
 from django.shortcuts import render
-from .dashboard import frequency_table, display_my_dataset, aggregated_df, crossed_table, pivot_table
+from .dashboard import (
+    frequency_table,
+    display_my_dataset,
+    aggregated_df,
+    crossed_table,
+    pivot_table,
+    visualize_sales_with_sunburst_chart,
+    visualize_sales_with_treemap,
+    visualize_manufacturer_bar_chart,
+    visualize_sales_with_icicle,
+    sales_by_country
+)
 from django.conf import settings
 
 def dashboard_view(request):
@@ -24,4 +35,9 @@ def dashboard_view(request):
         "aggregated_data": aggregated_df(df),
         "crossed_table": crossed_table(df),
         "pivot_table": pivot_table(df),
+        "sunburst_chart": visualize_sales_with_sunburst_chart(df),
+        "treemap_chart": visualize_sales_with_treemap(df),
+        "manufacturer_bar_chart": visualize_manufacturer_bar_chart(df),
+        "icicle_chart": visualize_sales_with_icicle(df),
+        "world_sales_map": sales_by_country(df),
     })
